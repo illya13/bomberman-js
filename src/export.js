889372178a00788c21fa51a2c8f0a03c87f8d772
Illya Havsiyevych <Illya_Havsiyevych@epam.com>
2020-07-01 11:50:22 +0300

    bulk export

--- src/export.js
+++ src/export.js
@@ -3,19 +3,38 @@ const fs = require('fs').promises
 const settings = require('./settings');
 const lib = require('./lib');
 
-const run = async (player) => {
-  const mongoClient = await lib.mongoClient();
-  const db = mongoClient.db(settings.mongo.dbName);
-  const fd = await fs.open(player+'.json', 'w');
+
+const exportPlayer = async (db, player) => {
+  const fd = await fs.open(player+'.raw', 'w');
 
   const cursor = await db.collection(player).find({});
-  for await(const boards of cursor) {
-    const data = lib.cleanAllBoards(boards);
+
+  let i = 0;
+  for await(const board of cursor) {
+    i++;
+    if (i % 1000 === 0) console.log(player, i);
+
+    const data = lib.cleanBoard(board);
     await fs.writeFile(fd, JSON.stringify(data)+"\n");
   }
   await fd.close()
+};
 
+
+const run = async () => {
+  const mongoClient = await lib.mongoClient();
+  const db = mongoClient.db(settings.mongo.dbName);
+  const collections = await db.collections();
+  for (const i in collections) {
+    const player = collections[i];
+    if (player.collectionName === 'boards') continue;
+
+    await exportPlayer(db, player.collectionName);
+  }
+
+  console.log("ALL DONE");
   await mongoClient.close();
 };
 
-(async () => await run('boards'))();
+
+(async () => await run())();
